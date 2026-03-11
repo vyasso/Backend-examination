3ed76a69591721d2a89ecd0cafb2dbf9230b8325
arok201001 <164240289+arok201001@users.noreply.github.com>
2026-03-12 00:53:30 +0100

    Postman instructions

--- src/schemas/validation.ts
+++ src/schemas/validation.ts
@@ -12,7 +12,6 @@ export const registerSchema = {
   }
 };
 
-// Regler för att skapa ett event
 export const createEventSchema = {
   schema: {
     body: {
@@ -26,3 +25,33 @@ export const createEventSchema = {
     }
   }
 };
+
+/*
+
+ * 1. REGISTRERA KONTO
+ * POST http://localhost:3000/api/auth/register
+ * Body: { "email": "test@test.se", "password": "password123" }
+ *
+ * 2. LOGGA IN
+ * POST http://localhost:3000/api/auth/login
+ * Body: { "email": "test@test.se", "password": "password123" }
+ * (Kopiera din accessToken och lägg under fliken Authorization -> Bearer Token)
+ *
+ * 3. SKAPA EVENT (Kräver Admin-token)
+ * POST http://localhost:3000/api/events
+ * Body: { "title": "Presentation", "description": "API Demo", "date": "2026-03-20T10:00:00Z" }
+ *
+ * 4. KOLLA PÅ EVENTS (Öppen för alla)
+ * GET http://localhost:3000/api/events
+ * Body: (tom)
+ *
+ * 5. TA BORT EVENT (Kräver Admin-token)
+ * DELETE http://localhost:3000/api/events/KLISTRA_IN_EVENT_ID_HÄR
+ * Body: (tom)
+ */
+
+/*
+Admin-konto:
+"email": "ronaldo@example.com",
+  "password": "mypassword123"
+*/
