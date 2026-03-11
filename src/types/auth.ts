8536337b509e7ee95e30e661d2406b86d1547c71
YASSO <89705636+vyasso@users.noreply.github.com>
2026-03-11 09:34:38 +0100

    Revert "gjort sanitization, authentication"

--- src/types/auth.ts
+++
@@ -1,9 +0,0 @@
-export type TokenPayload = {
-    sub: string;
-    role: "user" | "admin";
-    type: "access" | "refresh";
-    email: string;
-    display_name: string;
-    iat: number;
-    exp: number;
-  };
